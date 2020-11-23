f9aa72d5c2bccde8fa4a8184e3bce6a4410e0f45
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-11-24 00:06:47 +0100

    SearchBox added

---
+++ src/components/SearchBox/index.js
@@ -0,0 +1,17 @@
+import React ,{useState} from 'react'
+import {StyledSearchButton , StyledSearchWrapper , StyledSearchInput} from './SearchBox.style';
+
+export const SearchBox = ({setSearchKey}) => {
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+        <StyledSearchWrapper>
+            <StyledSearchInput color='#1f1f1f' isColored={false} onChange={(e)=>setInputValue(e.target.value)}/>
+            {/* // yukardaki e kısaltması evetn den gelmektedir */}
+            <StyledSearchButton onClick={() => setSearchKey()} >Search</StyledSearchButton>
+            
+        </StyledSearchWrapper>
+    )
+}
+
+ 
