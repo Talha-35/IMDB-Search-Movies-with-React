e0d1fb371450abf2c7bd889d67a7279fca719316
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-11-24 11:54:12 +0100

    App modified

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { SearchBox } from "./components/SearchBox";
 import axios from "axios";
 import "./App.css";
 
-const apiKey = "";
+const apiKey = "3e4dee1e85013c94ee13a705f4f72c22";
 const baseUrl = "https://api.themoviedb.org/3/search/movie";
 const baseImageUrl = "https://image.tmdb.org/t/p/w500";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
-  const [searchKey, setSearchKey] = useState('');
+  const [searchKey, setSearchKey] = useState('Titanic');
+  // yukardki searchkey usestateini boş strign bırakır isek sayfa ilk açılışta boş gelir. eğer b,r değer atar iseniz otomatik o sonuç ekranı gözükür
 
   // useEffect(()=>{
-  //     axios.get('https://api.themoviedb.org/3/search/movie?api_key={buraya key yazılır}&query=titanic&page=1')
+  //     axios.get('https://api.themoviedb.org/3/search/movie?api_key=3e4dee1e85013c94ee13a705f4f72c22&query=titanic&page=1')
   //         .then((res)=> setMovieList(res.data.results))
   //         .catch()
   //         .finally()
